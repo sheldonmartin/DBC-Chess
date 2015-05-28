f64a61cd89381be7047a505313ed840f8779af8c
Sheldon Martin <sheld0ri@gmail.com>
2015-05-28 15:34:30 -0400

    creates interface

---
+++ model/board.rb
@@ -0,0 +1,11 @@
+class Board
+  def initialize
+    @board # 8x8 board
+  end
+
+  # input: original coordinate of the piece being moved.
+  # output: N/A
+  # result: moves piece to new_coordinates
+  def move(original, new_coordinates)
+  end
+end
---
+++ view.rb
@@ -0,0 +1,12 @@
+module View
+  def self.welcome
+    # sends welcome message
+  end
+
+  def self.input
+    gets.chomp
+  end
+
+  def self.display_board(board)
+  end
+end

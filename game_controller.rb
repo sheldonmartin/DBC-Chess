a49536c1102e068e0ec3401b7daca4ba9ab322f7
Sihong31 <sihongspring2011@gmail.com>
2015-05-28 17:57:02 -0400

    view and controller additions

--- game_controller.rb
+++ game_controller.rb
@@ -1,2 +1,28 @@
+require_relative 'Board'
+
 class GameController
+
+
+  def initialize
+    @board = Board.new
+    View.welcome
+    run
+  end
+
+  def run
+    View.color
+    color = View.input
+    @board = initialize_board #places all pieces on the board.
+    View.first_move
+    View.choose_piece
+    piece = View.input
+    View.choose
+    old_location = View.input #a7
+    View.where_to(piece)
+    new_location = View.input
+    @board.move(old_location,new_location)
+
+
+  end
+
 end
--- view.rb
+++ view.rb
@@ -1,6 +1,6 @@
 module View
   def self.welcome
-    # sends welcome message
+    puts "Welcome to our awesome chess game!"
   end
 
   def self.input
@@ -8,5 +8,33 @@ module View
   end
 
   def self.display_board(board)
+  end
+
+  def self.color
+    puts "What color do you want to be? (Black or White)"
+  end
+
+  def self.first_move
+    puts "White goes first."
+  end
+
+  def self.choose_piece
+    puts "What piece do you want to move?"
+  end
+
+  def self.choose
+    puts "What is the location of the piece you want to move?"
+  end
+
+  def self.where_to(piece)
+    puts "Where do you want to move your #{piece} to?"
+  end
+
+  end
+  def self.piece_move(piece, origin, updated)
+    puts "You are moving #{piece} from #{origin} to #{updated}."
+  end
+
+
   end
 end

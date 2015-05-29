d1168ff5a49a95715b05534acd31a89c36b8633a
Sihong31 <sihongspring2011@gmail.com>
2015-05-29 16:12:38 -0400

    editted view and game controller with capture, invalid, loop comments

--- view.rb
+++ view.rb
@@ -1,4 +1,11 @@
 module View
+
+  def self.display_game_start
+    welcome
+    color
+    display_board(board)
+  end
+
   def self.welcome
     puts "Welcome to our awesome chess game!"
   end
@@ -12,31 +19,39 @@ module View
   end
 
   def self.color
-    puts "What color do you want to be? (Black or White)"
+    puts "What color do you want to be? (black or white)"
   end
 
-  def self.first_move
-    puts "White goes first."
+  def first_move?
+    puts "White goes first, what piece do you want to move?"
   end
 
-  def self.choose_piece
-    puts "What piece do you want to move?"
+  def self.where_from?(piece)
+    puts "What is the location of the #{piece} you want to move?"
   end
 
-  def self.choose
-    puts "What is the location of the piece you want to move?"
-  end
-
-  def self.where_to(piece)
+  def self.where_to?(piece)
     puts "Where do you want to move your #{piece} to?"
   end
-
 
   def self.piece_move(piece, current, updated)
     puts "You moved #{piece} from #{current} to #{updated}."
   end
 
+  def self.capture(color,piece)
+    puts "#{color} player captured #{piece}."
+  end
 
+  def self.invalid
+    puts "That is an invalid move."
+  end
 
+  def self.white_turn
+    puts "White player's turn."
+  end
+
+  def self.black_turn
+    puts "Black player's turn."
+  end
 
 end

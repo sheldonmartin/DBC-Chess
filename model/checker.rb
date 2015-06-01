1b654edb36e71ecfdf64eafdb3874c35b540d322
Sheldon Martin <sheld0ri@gmail.com>
2015-05-31 20:10:35 -0400

    adds file

---
+++ model/checker.rb
@@ -0,0 +1,27 @@
+require_relative "piece"
+
+class Checker < Piece
+  TYPE = "C"
+
+  def get_possible_coordinates(current)
+    possible_ary = []
+
+    count=1
+    while count <= 1 
+      possible_ary << [current[0]-count, current[1]-count]
+      possible_ary << [current[0]-count, current[1]+count]
+      possible_ary << [current[0]+count, current[1]+count]
+      possible_ary << [current[0]+count, current[1]-count]
+
+      count+=1
+    end
+
+    possible_ary.select do |coordinate|
+      if coordinate[0] > 7 || coordinate[0] < 0 || coordinate[1] > 7 || coordinate[1] < 0
+        false
+      else
+        true
+      end
+    end
+  end
+end
